c218afc888343717099ec327d625f98148500b25
Enrique <112001274+Tiorikao@users.noreply.github.com>
2026-06-29 05:38:37 -0300

    Adiciona entidade Usuario

---
+++ src/main/java/com/example/tarefaapi/model/Usuario.java
@@ -0,0 +1,70 @@
+package com.example.tarefaapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String role;
+
+    public Usuario() {
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (role == null || role.isBlank()) {
+            role = "CLIENTE";
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
